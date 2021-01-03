f39c32f961ca672ba27f4f087e989d6153a1038d
Sergey Gurman <sgurman753@gmail.com>
2021-01-03 12:08:16 -0500

    fixed bugs with experiences

--- SARPGaS/Model/experiences.py
+++ SARPGaS/Model/experiences.py
@@ -10,7 +10,11 @@ class Exp:
 
     @staticmethod
     def convert_to_xp(level):
-        return math.ceil((lambda x: (3 / 4) * (x ** 3))(level))
+        return (lambda x: (3 / 4) * (x ** 3))(level)
+
+    @staticmethod
+    def convert_to_level(xp):
+        return (lambda y: ((4 / 3) * y) ** (1/3))(xp)
 
     def level_up(self):
         try:
@@ -26,15 +30,29 @@ class Exp:
 
 if __name__ == "__main__":
     e = Exp()
-    for num in range(0, 10):
-        print(f"{num} : {e.convert_to_xp(num)}")
+    # for num in range(0, 10):
+    #     print(f"{num} : {e.convert_to_xp(num)}")
+    #
+    # print(e.table)
+    # e.xp = 2000
+    # while True:
+    #     if e.level_up():
+    #         print(e.level)
+    #     else:
+    #         print(e.level)
+    #         break
 
-    print(e.table)
-    e.xp = 2000
-    while True:
-        if e.level_up():
-            print(e.level)
-        else:
-            print(e.level)
-            break
+    # print(e.table)
+    # x = (lambda x: (x ** 3))(4)
+    # print(x)
+    # y = (lambda y: 1/(y ** -3))(4)
+    # print(y)
+
+    test = e.convert_to_xp(2)
+    print(test)
+    test = e.convert_to_level(test)
+    print(test)
+
+
+
 
--- SARPGaS/Unit_Test/test_experiences.py
+++ SARPGaS/Unit_Test/test_experiences.py
@@ -4,13 +4,41 @@ import SARPGaS.Model.experiences as exp
 
 class MyTestCase(unittest.TestCase):
     # y = 2x^2 + 3x + y
-    def test_xp_conversion(self):
+    # def test_xp_conversion(self):
+    #     xp = exp.Exp()
+    #     self.assertEqual(xp.level_to_exp(0), 0)
+    #     self.assertEqual(xp.level_to_exp(1), 5)
+    #     self.assertEqual(xp.level_to_exp(2), 19)
+    #     self.assertEqual(xp.level_to_exp(3), 46)
+    #     self.assertEqual(xp.level_to_exp(4), 90)
+
+    def test_level_to_level_1(self):  # Testing converting from level to xp then converting back to level
         xp = exp.Exp()
-        self.assertEqual(xp.level_to_exp(0), 0)
-        self.assertEqual(xp.level_to_exp(1), 5)
-        self.assertEqual(xp.level_to_exp(2), 19)
-        self.assertEqual(xp.level_to_exp(3), 46)
-        self.assertEqual(xp.level_to_exp(4), 90)
+        amount = xp.convert_to_xp(1)
+        self.assertEqual(amount, .75)
+        value = xp.convert_to_level(amount)
+        self.assertEqual(value, 1)
+
+    def test_level_to_level_4(self):  # Testing converting from level to xp then converting back to level
+        xp = exp.Exp()
+        amount = xp.convert_to_xp(4)
+        self.assertEqual(amount, 48)
+        value = xp.convert_to_level(amount)
+        self.assertEqual(value, 4)
+
+    def test_xp_to_xp_1(self):  # Testing converting from xp to level then converting back to xp
+        xp = exp.Exp()
+        amount = xp.convert_to_level(.75)
+        self.assertEqual(amount, 1)
+        value = xp.convert_to_xp(amount)
+        self.assertEqual(value, .75)
+
+    def test_xp_to_xp_4(self):  # Testing converting from xp to level then converting back to xp
+        xp = exp.Exp()
+        amount = xp.convert_to_xp(48)
+        self.assertEqual(amount, 4)
+        value = xp.convert_to_level(amount)
+        self.assertEqual(value, 48)
 
 
 if __name__ == '__main__':

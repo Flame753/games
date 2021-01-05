07d8fc59a5fa4ab1b8d3abf107caf2e51f444305
Sergey Gurman <sgurman753@gmail.com>
2021-01-05 12:25:07 -0500

    made a moving green square that takes keyboard input

--- SARPGaS/Game/game.py
+++ SARPGaS/Game/game.py
@@ -1,26 +1,19 @@
 # Pygame template - skeleton for a new pygame project
 import pygame
 import random
-
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
-
-# define colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+from SARPGaS.Player.player import Player
+from SARPGaS.Game.constance import SCREEN_WIDTH, SCREEN_HEIGHT, FPS, BLACK, WHITE, BLUE, RED, GREEN
 
 # initialize pygame and create window
 pygame.init()
 pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("SARPGaS")
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
 
 def ControllerTick():
@@ -32,10 +25,11 @@ def ControllerTick():
     return True
 
 
-def ViewTick():
+def ViewTick(pressed_keys):
     # Draw Everything
     # Update
-    all_sprites.update()
+    # Update the player sprite based on user keypresses
+    player.update(pressed_keys)
     # Draw / render
     screen.fill(BLACK)
     all_sprites.draw(screen)
@@ -49,9 +43,14 @@ def Main():
     while running:
         # keep loop running at the right speed
         clock.tick(FPS)
+        # Get the set of keys pressed and check for user input
+        pressed_keys = pygame.key.get_pressed()
+
         # Process input (event)
         if not ControllerTick():
             break
+
+        ViewTick(pressed_keys)
 
     pygame.quit()
 
